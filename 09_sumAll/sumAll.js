9cabf6a77e3de4e8a2e40dd2b85ef7eec71db4ec
Falcosn28 <205811111+Falcosn28@users.noreply.github.com>
2025-06-22 13:21:03 +0200

    my solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num01, num02) {
+  let arr = []
+  let lower
+  let higer
 
+  if (!Number.isInteger(num01) || !Number.isInteger(num02) || num01 < 0 || num02 < 0) {return "ERROR"}
+  if (num01 > num02) {lower = num02; higer = num01}
+  else if (num01 < num02) {lower = num01; higer = num02}
+
+  while (lower <= higer) {
+    arr.push(lower)
+    lower++
+  }
+  return arr.reduce((sum, cur) => sum + cur);
 };
 
 // Do not edit below this line
