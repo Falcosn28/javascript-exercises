6e8318bbab07a59bd5c30aeb5a2bf96b027dc0cc
Falcosn28 <205811111+Falcosn28@users.noreply.github.com>
2025-06-22 12:23:35 +0200

    my solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rm) {
+  for (const delet of rm) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === delet) {
+        arr.splice(i, 1)
+        i--
+      }
+    }
+  }
+  return arr
 };
 
 // Do not edit below this line
