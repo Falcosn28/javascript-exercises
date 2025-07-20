f1135a87751c21d7ec2c1f8b53193773892564e0
Falcosn28 <205811111+Falcosn28@users.noreply.github.com>
2025-07-21 01:06:25 +0200

    my solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const origin = str.replace(/[!., ]/g, '').toLowerCase().split("").join("")
+  const reverse = origin.split("").reverse().join("")
 
+  return origin == reverse ? true : false
 };
 
 // Do not edit below this line
