9c5c582f8703859430373fd327838cf45b987214
Falcosn28 <205811111+Falcosn28@users.noreply.github.com>
2025-07-22 00:41:23 +0200

    my solution

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let resulet = []
+  for (let i = 0; i < books.length; i++) {
+    resulet.push(books[i].title)
+  }
+  return resulet
 };
 
 // Do not edit below this line
